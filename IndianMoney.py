e0699d143d53565e8d717ddf13e5d507fb34e80a
aginjithgj <96783645+aginjithgj@users.noreply.github.com>
2022-09-23 17:16:59 +0530

    Create IndianMoney.py

---
+++ IndianMoney.py
@@ -0,0 +1,14 @@
+
+from numpy import number
+
+
+number = input("Enter the amount: ")
+
+
+def formatINR(number):
+    s, *d = str(number).partition(".")
+    r = ",".join([s[x-2:x] for x in range(-3, -len(s), -2)][::-1] + [s[-3:]])
+    return "".join([r] + d)
+
+
+print(formatINR(number))
